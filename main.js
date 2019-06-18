c5d42e7a7dda982eeec76cf947eb47022d304ca8
Angie Burk <redpear@mac.com>
2019-06-18 06:56:03 -0700

    New changes to BST Map
    Added key value pairs, and console.log to see the values

--- main.js
+++ main.js
@@ -1,18 +1,8 @@
-//console.log('hello?');
+/*
 
-/*let map = new Map();
-map.set("root", "name")
-map.set("simple", "dimple");
-map.set("sally", "sunshine");
-map.set("sandy","beach");
-map.set("things", "apples");
-map.set("uncles", "aunts");
-map.set("windy", "town");
+    Map in a Binary Search Tree
+
 */
-
-
-//console.log(map.get('root'));
-//console.log(map.get('simple'));
 
 const map = new Map();
       map.set('root', 'name')
@@ -27,38 +17,60 @@ const iterator = map.entries();
 //console.log(map.set(['root'], 'name'));
 
 class BinarySearchTree {
-    constructor(){
+    constructor(key, value){
         this.left = null;
         this.right = null;
         this.root = null;
+        this.key = key;
+        this.value = value;
     }
 
     insert(key, value) {
-        console.log(`Key passed in: ${key}  value: passed in ${value}`);
+        //console.log(`Key passed in: ${key}  value: passed in ${value}`);
         for(let item of map.entries()){
-            
-            if(this.root === null){
-                this.root = item[0];
-                console.log('Root' + ' ' + this.root);
-                //console.log(`Item ${item}`);
-            }
-            if(key < this.root && this.left){
-                console.log("Hello " + ' ' + this.insert(item));
-                //console.log('Root ' + ' ' + this.root);
-                //console.log('Insert Left: ' + ' ' + map.set(key, value));
-                //console.log(`the insert left` + ' ' + this.left.insert(map.set(key, value)));
-                console.log(item[0]);
-             } else if(key < item[0]){
-                 console.log("key" + ' ' + key);
-                 console.log("this.key" + " " + item[0]);
-                 //this.left = map.set(key, value);
-             } else if(key > this.root && this.right){
-                 console.log(this.right.insert(item));
-             } //else if(key > item){
-            //     this.right = map.set(key, value);
-            // } else {
-            //     return null;
-            // }
+                this.key = item[0];
+                this.value = item[1];
+
+                if(this.root === null){
+                    this.root = item[0];
+                    console.log('Root' + ' ' + this.root);
+                }
+                if(key < item[0] && this.left && this.root !== null){
+                    
+                    this.left = this.insert(this.key, this.value);
+                    
+                    console.log("this.left key: " + ' ' + item[0]);
+                    console.log("this.left value: " + ' ' + item[1]);
+                    console.log("insert left:" + ' ' +  map.set(this.key, this.value));
+                    // this.insert(map.set(this.key, this.value)));
+
+                } else if(key < item[0] && this.root !== null){
+                
+                    console.log("this.left 2 key" + ' ' + this.key);
+                    console.log("this.value 2 value" + " " + this.value);
+
+                this.left = map.set(this.key, this.value);
+                
+                console.log("this.left = " + ' ' + this.left);
+                console.log('left new Node' + ' ' + map.set(this.key, this.value));
+                 
+             } else if(key > item[0] && this.right && this.root !== null){
+                 
+                 console.log(this.insert(map.set(this.key, this.value)));
+                 console.log("this.right key" + ' ' + )
+                 console.log('right insert node: ' + ' ' + map.set(this.key, this.value));
+
+                 // this.right.insert(map.set(this.key, this.value
+             } else if(key > item && this.root !== null){
+
+                this.right = map.set(this.key, this.value);
+
+                console.log("this.right: " + ' ' + this.right);
+                console.log("new right node: " + ' ' + map.set(this.key, this.value));
+
+             } else {
+                 return null;
+             }
 
             // for(let ent of map.entries()){
             //     console.log('Key' + ' ' + ent + ' ' + '' + 'Value: ' + ' ' + iterator.next().value);
@@ -67,8 +79,8 @@ class BinarySearchTree {
     }
 }
 
-const bst = new BinarySearchTree();
-console.log(bst.insert('just', 'this story for the shelves'.toString()));
+const bst = new BinarySearchTree('root', 'name');
+console.log(bst.insert('just', 'this story for the shelves'));
 
 
 
