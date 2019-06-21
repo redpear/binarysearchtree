8cbcb02b8938f5cac4da054175b0e3e5971f740a
Angie Burk <redpear@mac.com>
2019-06-21 15:31:22 -0700

    linedlist
    linkedlist addToHead, AddToTail and jest tesing

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // constructor functions
 
-// function LinkedList() {
-//     this.head = null;
-//     this.tail = null;
-// }
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+}
 
     
 
@@ -15,25 +15,35 @@ function Node(value, next, prev) {
 
 
 
-class LinkedList {
-    constructor() {
-      this.head = null;
-      this.tail = null;
-    }
+// class LinkedList {
+//     constructor() {
+//       this.head = null;
+//       this.tail = null;
+//     }
+// }
 
-    addToHead(value) {
-        let newNode = new Node(value, this.head, null)
 
-        //if the head exists
-        if(this.head){
-            this.head.prev = newNode;
-        } else {    // no nodes and list is empty
-            this.tail = newNode;
-        }  
-        this.head = newNode;
-    }
+LinkedList.prototype.addToHead = function(value) {
+    let newNode = new Node(value, this.head, null)
+
+    //if the head exists
+    if(this.head){
+        this.head.prev = newNode;
+    } else {    // no nodes and list is empty
+        this.tail = newNode;
+    }  
+    this.head = newNode;
 }
 
+LinkedList.prototype.addToTail = function(value){
+    let newNode = new Node(value, null, this.tail);
+    if(this.tail)    //if the ll is not empty   // is the linked list empty or not
+        this.tail.next = newNode;
+    else
+        this.head = newNode;    // if the ll is empty create a head node
+
+        this.tail = newNode;    // return tail node
+}
 
 // class Node {
 //     constructor(value, next, prev) {
@@ -56,13 +66,34 @@ class LinkedList {
 // }
 
 
-
-
-
-// test - 3 
+// test - 6 addToTail()
 const linkedlist = new LinkedList();
-linkedlist.addToHead(100);
-console.log(linkedlist);
+linkedlist.addToTail(10);
+linkedlist.addToHead(20);
+linkedlist.addToHead(30);
+
+console.log(linkedlist.head.next.prev);
+console.log(linkedlist.tail.prev.next);
+//console.log(link)
+
+// test - 5 addToTail()
+// const linkedlist = new LinkedList();
+// linkedlist.addToTail(10);
+// linkedlist.addToHead(20);
+
+// console.log(linkedlist);
+
+// test - 4 addToTail()
+// const linkedlist = new LinkedList();
+// linkedlist.addToTail(10);
+// console.log(linkedlist);
+
+// test - 3 addToHead()
+// const linkedlist = new LinkedList();
+// linkedlist.addToHead(100);
+// linkedlist.addToHead(200);
+// linkedlist.addToHead(300);
+// console.log(linkedlist);
 
 // test 2 - Test Node constructor function
 //let node = new Node(100, 'node2', null); // first node, so no previous node
--- test.js
+++ test.js
@@ -2,24 +2,104 @@ const L = require('./main');
 const LinkedList = L.LinkedList;
 const Node = L.Node;
 
+// LinkedList constructor test
 test('Tests the Constructor function for LinkedList', () => {
   expect(typeof LinkedList.prototype.constructor).toEqual('function');
 });
 
+// Node constructor test
 test('Node is a class', () => {
     expect(typeof Node.prototype.constructor).toEqual('function');
   });
 
-  // test - 3 
-// const linkedlist = new LinkedList();
-// linkedlist.addToHead(100);
-// console.log(linkedlist);
-
-
+  
+// test 3 addToHead
 describe('Insert First node', () => {
     test('adds a new node, the one node is the head and the tail', () => {
-        const linkedLlist = new LinkedList();
+      const linkedLlist = new LinkedList();
         linkedLlist.addToHead(100);
-        expect(linkedLlist.head.value).toEqual(100);
+        linkedLlist.addToHead(200);
+        
+        expect(linkedLlist.head.value).toEqual(200)
+        expect(linkedLlist.tail.value).toEqual(100);
     });
 });
+
+// test 4 addToHead
+describe.skip('Insert a second node', () => {
+    test('adds a new node to existing node, the two nodes: head and the tail', () => {
+      const linkedLlist = new LinkedList();
+      
+      const headNode = new Node(300, 200, null);
+    //   const headNext = new Node(200, 100, 300);
+    //   const tailPrev = new Node(100, 300, 200);
+      //const tailPrev = new Node(100, null, new Node(200, 100, 300));
+      
+      linkedlist.addToHead(100);
+      linkedlist.addToHead(200);
+      linkedlist.addToHead(300);
+        
+
+        // expect(linkedLlist.head.prev).toEqual(null);
+        // expect(linkedLlist.head.next).toEqual(200);
+        // expect(linkedLlist.head.value).toEqual(300);
+
+        expected(linkedLlist.head).toEqual(headNode)
+        
+    });
+});
+
+describe('test to try this another way', () => {
+    test('adds new nodes to the linkedlist', () => {
+        const linkedLlist = new LinkedList();
+        
+        linkedLlist.addToHead(100)
+        linkedLlist.addToHead(200)
+
+        expect(linkedLlist.head.value).toEqual(200);
+        expect(linkedLlist.tail.value).toEqual(100);
+    });
+});
+
+// test 6 addToTail
+describe('Insert tail node', () => {
+    test('adds a tail node, the one node is the head and the tail', () => {
+      const linkedLlist = new LinkedList();
+        linkedLlist.addToTail(10);
+        
+        expect(linkedLlist.head.value).toEqual(10)
+        
+    });
+});
+
+// test 7 addToTail
+describe('Insert tail node', () => {
+    test('tests the addToTail()', () => {
+      const linkedLlist = new LinkedList();
+
+        linkedLlist.addToTail(10);
+        //expect(linkedLlist.head).toEqual(new Node(null, null, 10));
+        expect(linkedLlist.tail.value).toEqual(10);
+
+        expect(linkedLlist.head).toEqual(new Node(10, null, null));
+        expect(linkedLlist.tail).toEqual(new Node(10, null, null));
+    });
+});
+
+// test 8 addToHead and addToTail
+describe.skip('Insert a head node and a tail node', () => {
+    test('tests addToHead and addToTail()', () => {
+      const linkedLlist = new LinkedList();
+
+        linkedLlist.addToHead(20);
+        linkedLlist.addToTail(10);
+
+        expect(linkedLlist.head.value).toEqual(20);
+        expect(linkedLlist.tail.value).toEqual(10);
+
+        expect(linkedLlist.head).toEqual(new Node(20, null, new Node(10, null, null)));
+        expect(linkedLlist.tail).toEqual(new Node(10, null, null));
+        
+    });
+});
+
